c50ff4cf34878416641b0615f7661ce3ad250ac8
Vincent Huang <vincom2@gmail.com>
2014-08-11 03:45:57 -0400

    echo server works!
    still slightly confuzzled by some of the types but AW YEAH
    (also man, stop calling unwrap(). the ML programmer in me is ashamed...)

--- main.rs
+++ main.rs
@@ -1,12 +1,18 @@
-use std::io::{TcpListener, TcpStream};
-use std::io::net::tcp::TcpAcceptor;
-use std::io::{Acceptor, Listener};
-use std::io::IoResult;
+use std::str;
+use std::io::{TcpListener, Acceptor, Listener};
 
 fn main() {
-    let mut acceptor = TcpListener::bind("127.0.0.1", 80).listen().unwrap();
-    let stream : Option<IoResult<TcpStream>> = acceptor.incoming().next();
-    //for stream in acceptor.incoming() {
-    //}
+    let mut acceptor = TcpListener::bind("127.0.0.1", 60002).listen();
+    for stream in acceptor.incoming() {
+        match stream {
+            Err(e) => fail!("died with exception: {}", e),
+            Ok(mut in_stream) => {
+                let mut out_stream = in_stream.clone();
+                let msg = in_stream.read_to_end().unwrap();
+                print!("{}", str::from_utf8(msg.as_slice()).unwrap());
+                out_stream.write(msg.as_slice());
+            }
+        }
+    }
 }
 
