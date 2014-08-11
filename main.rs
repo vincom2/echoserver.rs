b7fdda9328f64ed2d4b6d3978e9bcbf5c44dd3dc
Vincent Huang <vincom2@gmail.com>
2014-08-11 04:03:34 -0400

    replaced match with unwrap()
    yes yes is bad but all I was doing was calling fail!() anyway,
    which is what unwrap() does

--- main.rs
+++ main.rs
@@ -4,15 +4,11 @@ use std::io::{TcpListener, Acceptor, Listener};
 fn main() {
     let mut acceptor = TcpListener::bind("127.0.0.1", 60002).listen();
     for stream in acceptor.incoming() {
-        match stream {
-            Err(e) => fail!("died with exception: {}", e),
-            Ok(mut in_stream) => {
-                let mut out_stream = in_stream.clone();
-                let msg = in_stream.read_to_end().unwrap();
-                print!("{}", str::from_utf8(msg.as_slice()).unwrap());
-                out_stream.write(msg.as_slice());
-            }
-        }
+        let mut in_stream = stream.unwrap();
+        let mut out_stream = in_stream.clone();
+        let msg = in_stream.read_to_end().unwrap();
+        print!("{}", str::from_utf8(msg.as_slice()).unwrap());
+        out_stream.write(msg.as_slice()).unwrap();
     }
 }
 
