8c0d4071797c306c514d6c96d747a0a84dc3874a
Vincent Huang <vincom2@gmail.com>
2014-08-11 05:22:00 -0400

    added command-line options
    -h --help: displays help
    -p --port: enter port number

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
+extern crate getopts;
+use getopts::{optopt, optflag, getopts, OptGroup};
 use std::str;
 use std::io::{TcpListener, Acceptor, Listener};
+use std::os;
+
+fn print_usage(program: &str, opts: &[OptGroup]) {
+    println!("Usage: {} [options]", program);
+    for &o in opts.iter() {
+        //should print to string first in case option has no name?
+        println!("-{} --{}\t{}", o.short_name, o.long_name, o.desc);
+    }
+}
 
 fn main() {
-    let mut acceptor = TcpListener::bind("127.0.0.1", 60002).listen();
+    let args = os::args();
+    let program = args[0].clone();
+    let opts = [ optopt("p", "port", "port to listen on (default 60002)", "NUMBER"),
+                 optflag("h", "help", "print this") ];
+    let matches = getopts(args.tail(), opts).unwrap();
+    if matches.opt_present("h") {
+        print_usage(program.as_slice(), opts);
+        return;
+    }
+    let port : u16= match matches.opt_str("p") {
+        None => 60002,
+        Some(p) => from_str(p.as_slice()).unwrap()
+    };
+
+    let mut acceptor = TcpListener::bind("127.0.0.1", port).listen();
     for stream in acceptor.incoming() {
         let mut in_stream = stream.unwrap();
         let mut out_stream = in_stream.clone();
